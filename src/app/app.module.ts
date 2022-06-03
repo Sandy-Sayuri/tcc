909cf36482ab730c2ba53b03b3824edec99d1c22
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2022-06-03 10:43:56 -0300

    login atulização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { getPaginatorIntl } from './paginator';
 import { LoginComponent } from './security/login/login.component';
+import { LoginService } from './shared/services/login.service';
 
 @NgModule({
   declarations: [
@@ -15,9 +21,14 @@ import { LoginComponent } from './security/login/login.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule, 
+    HttpClientModule,
+    MaterialModule,
+    ReactiveFormsModule,
     ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [ LoginService,
+    {provide: MatPaginatorIntl, useValue: getPaginatorIntl()}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Usuario } from 'src/app/shared/model/user.module';
+import { LoginService } from 'src/app/shared/services/login.service';
+import { environment } from 'src/environments/environment';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  loginForm: FormGroup
+  redirectTo: string
+  validacao: boolean = false
+  hideSenha:boolean = true
+  msgErro: string
+  constructor(public loginService: LoginService,
+              private fb: FormBuilder,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) {
   }
-
+  ngOnInit() {
+    Swal.close()
+    this.loginForm = this.fb.group({
+      username: this.fb.control('', [Validators.required]),
+      password: this.fb.control('', [Validators.required]),
+      conected: this.fb.control(''),
+    })
+    this.redirectTo = this.activatedRoute.snapshot.params['to'] || btoa('dashboard/home')
+  }
+  login(usuario: Usuario) {
+    let login = this.loginService.login(usuario).subscribe({ 
+      next: (retorno:any)=>{      
+        if(retorno["errors"] != undefined){
+          this.validacao = true
+        } else {
+          if(retorno["tipo"] == 2){
+            this.msgErro = "Usuário sem acesso!"
+            this.validacao = true
+          } else {
+            localStorage.setItem(`${environment.STORAGE_NAME}:Token`, JSON.stringify(retorno.access_token))
+            // this.loginService.refresh()          
+            this.router.navigate(['dashboard'])
+          }
+        }
+      }, 
+      error: ()=>{
+        this.msgErro = "Usuário ou senha inválida!"
+        this.validacao = true
+        console.log("error") 
+      }, 
+      complete: ()=>{ 
+        login.unsubscribe()  
+      }
+    })
+  }
 }
